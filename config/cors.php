f3b110867aec0ef9aed97ff2831171edc1899c03
iqbalimaduddin99 <iqbalimaduddin8@gmail.com>
2025-05-25 07:08:37 +0700

    add cors

---
+++ config/cors.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'allowed_origins' => [
+        'https://simple-add-data-fe-production.up.railway.app/',
+    ],
+];
--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,18 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthController;
 
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
 
-Route::middleware('auth:sanctum')->get('categories', [CategoryController::class, 'index']);
-
-Route::middleware('auth:sanctum')->get('transactions/filter-data', [TransactionController::class, 'filter']);
-Route::middleware('auth:sanctum')->get('transactions/rekap', [TransactionController::class, 'rekap']);
-Route::middleware('auth:sanctum')->apiResource('transactions', TransactionController::class)->whereNumber('transaction');
+Route::middleware([Cors::class])->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
+    
+    Route::middleware('auth:sanctum')->get('categories', [CategoryController::class, 'index']);
+    
+    Route::middleware('auth:sanctum')->get('transactions/filter-data', [TransactionController::class, 'filter']);
+    Route::middleware('auth:sanctum')->get('transactions/rekap', [TransactionController::class, 'rekap']);
+    Route::middleware('auth:sanctum')->apiResource('transactions', TransactionController::class)->whereNumber('transaction');
+});
